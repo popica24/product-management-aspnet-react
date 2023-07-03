f84a1695485a2e5a61c1f248c8648609e4cd2d6f
Popica <78389957+popica24@users.noreply.github.com>
2023-07-03 11:35:44 +0300

    Add project files.

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using Business.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoriesController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        [HttpGet("categories/{offset?}/{limit?}")]
+        public List<CategoryModel> Get([FromRoute]int offset = 0, [FromRoute]int limit = 100)
+        {
+            return new List<CategoryModel>();
+        }
+    }
+
+   
+}
